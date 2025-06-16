b2e23fe6803f1dc95d6c815582b7d8d0471cef5d
magicdawn <magicdawn@qq.com>
2025-06-17 05:23:47 +0800

    chore: add format

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # purple-colors
-purple-colors
+
+https://magicdawn.github.io/purple-colors/
+
+## Credits
+
+Generated with ChatGPT 4o
---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import { fromSxzz } from '@magicdawn/eslint-config'
+export default fromSxzz(undefined, [
+  {
+    ignores: ['bun.lock'],
+  },
+])
