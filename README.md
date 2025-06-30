8e83562cc9caa3d6413e76d8f9c388ced9cdb9f8
magicdawn <magicdawn@qq.com>
2025-06-30 20:53:26 +0800

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# purple-colors
-
-https://magicdawn.github.io/purple-colors/
-
-## Credits
-
-Generated with ChatGPT 4o
--- eslint.config.js
+++
@@ -1,6 +0,0 @@
-import { fromSxzz } from '@magicdawn/eslint-config'
-export default fromSxzz(undefined, [
-  {
-    ignores: ['bun.lock'],
-  },
-])
