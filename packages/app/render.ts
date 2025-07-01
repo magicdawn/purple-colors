d00aa1286259407b84857332a324ca1cd2ee7358
magicdawn <magicdawn@qq.com>
2025-07-01 22:39:37 +0800

    chore: add badges to html

--- packages/app/render.ts
+++ packages/app/render.ts
@@ -2,5 +2,20 @@ import edge from 'edge.js'
 import fse from 'fs-extra'
 import { PURPLE_COLORS } from 'purple-colors'
 
-const html = await edge.render(`${import.meta.dirname}/index.html.edge`, { PURPLE_COLORS })
-fse.outputFileSync(`${import.meta.dirname}/dist/index.html`, html)
+async function renderFile() {
+  const html = await edge.render(`${import.meta.dirname}/index.html.edge`, { PURPLE_COLORS })
+  fse.outputFileSync(`${import.meta.dirname}/dist/index.html`, html)
+  console.log('[rendered]:', `${import.meta.dirname}/dist/index.html`)
+}
+
+function watch() {
+  console.log('[watching]:', `${import.meta.dirname}/index.html.edge`)
+  fse.watchFile(`${import.meta.dirname}/index.html.edge`, () => {
+    renderFile()
+  })
+}
+
+await renderFile()
+if (process.argv.some((arg) => arg === '-w' || arg === '--watch')) {
+  watch()
+}
